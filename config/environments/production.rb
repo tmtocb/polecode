2bbb6b67fd5574db58933836cc4c4921d069b5b0
Mateusz Tomczak <kittz.esport@gmail.com>
2022-04-16 15:19:18 +0200

    polecode[13] - force production access over SSL, use Strict-Transport-Security, and use secure cookies

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,8 @@
 require "active_support/core_ext/integer/time"
+# Settings here will take precedence over those in config/application.rb.
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  config.force_ssl = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -39,9 +40,6 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
