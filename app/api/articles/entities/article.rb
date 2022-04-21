16315296b201df6db4c408215382e88ec6f7a1d1
Mateusz Tomczak <kittz.esport@gmail.com>
2022-04-21 09:15:51 +0200

    polecode[41] - add Article entity, factory & spec, delegate DB fields

--- app/api/articles/entities/article.rb
+++ app/api/articles/entities/article.rb
@@ -1,11 +1,11 @@
 module Articles
-	module Entities
-		class Article
-			delegate :title, :short_desc, :full_desc, to: :@article
+  module Entities
+    class Article
+      delegate :title, :short_desc, :full_desc, to: :@article
 
-			def initialize(article)
-				@article = article
-			end
-		end
-	end
+      def initialize(article)
+        @article = article
+      end
+    end
+  end
 end
