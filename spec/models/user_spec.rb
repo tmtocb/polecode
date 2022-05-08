067121b2f0094a531905915e256302232cbd6bad
Mateusz Tomczak <kittz.esport@gmail.com>
2022-05-08 17:04:55 +0200

    add Devise User model, DB migrations, model specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe User do
+  describe 'database columns' do
+    it { should have_db_column(:email).of_type(:string).with_options(null: false, default: "") }
+    it { should have_db_column(:encrypted_password).of_type(:string).with_options(null: false, default: "") }
+    it { should have_db_column(:reset_password_token).of_type(:string).with_options(null: true) }
+    it { should have_db_column(:reset_password_sent_at).of_type(:datetime).with_options(null: true) }
+    it { should have_db_column(:remember_created_at).of_type(:datetime).with_options(null: true) }
+    it { should have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+    it { should have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
+  end
+
+  describe 'database indexes' do
+    it { should have_db_index(:email).unique(true) }
+    it { should have_db_index(:reset_password_token).unique(true) }
+  end
+end
